382ef640f35d98464321133f79ba1bd7a95023d5
JonasJallet <jonas.jallet@gmail.com>
2024-02-07 00:25:26 +0100

    fix: createMany for place mock

--- back-end/src/mocks/placeMock.ts
+++ back-end/src/mocks/placeMock.ts
@@ -5,7 +5,7 @@ import { PlaceMockFactory } from "../factories/placeMockFactory";
 export async function createPlaceMock() {
   await DataSource.initialize();
   const placeRepository = DataSource.getRepository(Place);
-  const placeData = await new PlaceMockFactory().create();
+  const placeData = await new PlaceMockFactory().createMany(20);
   await placeRepository.save(placeData);
   process.stdout.write("Generated Places Data saved to the database.");
 }
