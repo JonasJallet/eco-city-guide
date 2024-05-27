259892f01d1c8ee4ce12f7a6bc8c16bcc48fb0c3
JonasJallet <jonas.jallet@gmail.com>
2024-05-28 00:24:50 +0200

    Feat Fullscreen button

--- web-app/src/components/home/FullScreenButton.tsx
+++ web-app/src/components/home/FullScreenButton.tsx
@@ -4,21 +4,30 @@ import { FiMaximize, FiMinimize } from "react-icons/fi";
 const FullscreenButton = () => {
   const [isFullscreen, setIsFullscreen] = useState(false);
 
-  const toggleFullscreen = () => {
+  const toggleFullscreen = async () => {
     if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen().then(() => {
-        setIsFullscreen(true);
-      });
+      await document.documentElement.requestFullscreen();
+      setIsFullscreen(true);
     } else {
-      document.exitFullscreen().then(() => {
-        setIsFullscreen(false);
-      });
+      await document.exitFullscreen();
+      setIsFullscreen(false);
     }
   };
 
   return (
-    <div className="fullscreen-button">
-      <button onClick={toggleFullscreen}>
+    <div
+      className="leaflet-bar leaflet-control mt-2"
+      style={{
+        marginLeft: "10px",
+        borderRadius: "15px",
+        borderColor: "white",
+      }}
+    >
+      <button
+        className="text-xl text-primary_color bg-tertiary_color hover:bg-primary_color hover:text-tertiary_color rounded-xl flex justify-center items-center"
+        onClick={() => toggleFullscreen()}
+        style={{ width: "38px", height: "38px", lineHeight: "30px" }}
+      >
         {isFullscreen ? <FiMinimize /> : <FiMaximize />}
       </button>
     </div>
