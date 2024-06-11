6ff0e7b44deb3f580ef4f7b51b22a5e38872abbf
JonasJallet <jonas.jallet@gmail.com>
2024-06-11 15:14:31 +0200

    feat role logical

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -14,8 +14,16 @@ import { CityResolver } from "./resolvers/cityResolver";
 
 export type Context = { res: Response; user: User | null };
 
-const authChecker: AuthChecker<Context> = ({ context }) => {
-  return Boolean(context.user);
+const authChecker: AuthChecker<Context> = ({ context }, roles) => {
+  if (!context.user) {
+    return false;
+  }
+
+  if (roles.length === 0) {
+    return true;
+  }
+
+  return roles.includes(context.user.role);
 };
 
 const PORT = 4000;
